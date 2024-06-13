3990c0f8f86055166998ac0b829276d82facf90a
Leonardo Nunes <leonardonunes1314@gmail.com>
2024-06-13 12:21:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+let studentList = [{
+
+    name:`Leonardo`,
+    firstNote:9.6,
+    secondNote:9.8,
+},
+{
+    name:`Luiza`,
+    firstNote:5.6,
+    secondNote:6.8,
+},
+{
+    name:`Jorge`,
+    firstNote:4.2,
+    secondNote:4.8,
+},
+{
+    name:`Matheus`,
+    firstNote:8.0,
+    secondNote:9.4,
+}
+]
+
+function sumOfNotes(list){
+    
+    for(let i = 0 ; i < list.length;i++){
+        let total= (list[i].firstNote + list[i].secondNote) /2 .toFixed(2);
+        total= total.toFixed(1)
+        if( total >=7 ){
+            alert(`Á média do aluno(a) ${list[i].name} é ${total} 
+                Parabéns ${list[i].name},você foi aprovado(a)`)
+        }else{
+            alert(`Á média do aluno(a) ${list[i].name} é ${total} 
+                Que pena ${list[i].name},você foi reprovado(a)`)
+        }
+    }
+}
+
+sumOfNotes(studentList)
